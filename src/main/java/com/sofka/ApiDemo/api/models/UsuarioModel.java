6443c58192ebe41e4fa3081fc5c5baa78dea5820
Lukas-jb <92315994+Lukas-jb@users.noreply.github.com>
2021-11-28 18:43:58 -0500

    Se modifica Aplication.propertis, UsuarioModel
     Aplication.propertis se modifica informacion para la coneccion a la base de datos
    Se refactoriza UsuarioModel

--- src/main/java/com/sofka/ApiDemo/api/models/UsuarioModel.java
+++ src/main/java/com/sofka/ApiDemo/api/models/UsuarioModel.java
@@ -11,30 +11,32 @@ public class UsuarioModel {
     @Column(unique = true, nullable = false)
     private Long id;
 
+    @Column(nullable = false)
     private String nombre;
+
+    @Column(unique = true, nullable = false)
     private String email;
+
+    @Column(nullable = false)
     private Integer prioridad;
-
-    public void setPrioridad(Integer prioridad){
-        this.prioridad = prioridad;
-    }
-
-    public Integer getPrioridad(){
-        return prioridad;
-    }
 
     public Long getId() {
         return id;
     }
-
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public void setPrioridad(Integer prioridad){
+        this.prioridad = prioridad;
+    }
+    public Integer getPrioridad(){
+        return prioridad;
     }
 
     public String getNombre() {
         return nombre;
     }
-
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
@@ -42,7 +44,6 @@ public class UsuarioModel {
     public String getEmail() {
         return email;
     }
-
     public void setEmail(String email) {
         this.email = email;
     }
